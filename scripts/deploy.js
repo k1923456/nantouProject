8aef12eb1df4230e26fc899671395074249ba59b
santai-wang-7senses <88714960+santai-wang-7senses@users.noreply.github.com>
2021-12-01 19:34:17 +0800

    feat: Add deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,25 +1,25 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const accounts = await hre.ethers.getSigners();
+  const Item1Data = {
+    shid: 1234,
+    organizationID: 1,
+    producedDate: 1637857385,
+    expirationDate: 2637857385,
+    organization: accounts[0].address,
+    name: "茶葉",
+    organizationName: "8D Tea",
+  };
+  const number = 1500;
+  const packnumber = 5;
+  const unit = "ML";
+  const Item = await hre.ethers.getContractFactory("Item");
+  const item = await Item.deploy(Item1Data, number, packnumber, unit);
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  await item.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Item deployed to:", item.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
